f7dd582f8f795b19a645c9104a0ed173b3bb231e
pm96 <pm96@live.no>
2019-08-30 14:12:23 +0200

    added semantic ui css for project. removed react-specific pictures

--- shared_music/src/App.js
+++ shared_music/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import DescriptionBar from './components/DescriptionBar';
+import ListItem from './components/ListItem';
+import Searchbar from './components/Searchbar';
+import SongList from './components/SongList';
+import { Container, Header, } from 'semantic-ui-react';
 
 class App extends React.Component {
 
-  
+
   render(){
     return(
       <div>
-        <p>App</p>
-      </div>
+        <Container text>
+          <Header as='h2'>Music Share</Header>
+          <Searchbar />
+
+        </Container>
+      </div> 
     );
   }
 
--- shared_music/src/components/searchbar.js
+++ shared_music/src/components/searchbar.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+class SearchBar extends React.Component{
+
+    render(){
+        return(
+            <p>
+                SearchBar
+            </p>
+        );
+    }
+}
+
+export default SearchBar;
