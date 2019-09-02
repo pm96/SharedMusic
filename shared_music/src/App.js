e862a20e5be0f4b163a097a9636e35463420cc2d
Thomas Guest <thomas_guest@hotmail.com>
2019-09-02 09:35:31 +0200

    Started to work on structure and layout for playlist and list-items

--- shared_music/src/App.js
+++ shared_music/src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import './App.css';
+import Playlist from './components/SongList';
+import ListItem from './components/ListItem';
+
+const playList = [
+  {
+    title: "Michael Jackson - Beat it",
+    uploader: "Jokestar64",
+    length: "3:43"
+  }, 
+  {
+    title: "Kygo - Firestone",
+    uploader: "noob_master69",
+    length: "4:27"
+  },
+  {
+    title: "Sean Paul - Press it up",
+    uploader: "askSteve",
+    length: "3:54"
+  },
+]
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      list: playList,
+
+    }
+
+  }
+
+  componentDidMount() {
+    console.log("Mounted..")
+  }
+
+  addToList = () => {
+    // add song to playlist
+  }
+
+  removeFromList = () => {
+    // remove song from playlist
+  }
   
   render(){
     return(
       <div>
-        <p>App</p>
+        <Playlist playlist={this.state.list} />
+        
       </div>
     );
   }
